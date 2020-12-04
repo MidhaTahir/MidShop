229007dd862b153f273b7387a49cedcca3f2e2c2
MidhaTahir <midhatahirkhan2011@gmail.com>
2020-12-04 12:04:44 +0500

    added middlewares

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import productRouter from "./routes/productRouter.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
-connectDB()
+connectDB();
 
 const app = express();
 app.use(
@@ -15,17 +16,22 @@ app.use(
     credentials: true,
   })
 );
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use('/api/products/',productRouter)
+app.use("/api/products/", productRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
 app.listen(
   port,
   console.log(
-    `Server has started to run on ${port} in ${process.env.NODE_ENV} mode`.bgGreen
+    `Server has started to run on ${port} in ${process.env.NODE_ENV} mode`
+      .bgGreen
   )
 );
